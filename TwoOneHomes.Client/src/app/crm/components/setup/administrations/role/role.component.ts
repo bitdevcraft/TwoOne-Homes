f29002de77ab5f6d52df200cbf7da85da18be6e8
The Bit Dev <ryanvincent.capio@outlook.com>
2024-12-13 10:50:45 +0400

    Updated the Web Role ui

--- TwoOneHomes.Client/src/app/crm/components/setup/administrations/role/role.component.ts
+++ TwoOneHomes.Client/src/app/crm/components/setup/administrations/role/role.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Button} from "primeng/button";
+import {ButtonGroupModule} from "primeng/buttongroup";
+import {PrimeTemplate} from "primeng/api";
+import {RouterLink} from "@angular/router";
+import {TableModule} from "primeng/table";
+
+interface Column {
+  field: string;
+  header: string;
+}
 
 @Component({
   selector: 'app-role',
   standalone: true,
-  imports: [],
+  imports: [
+    Button,
+    ButtonGroupModule,
+    PrimeTemplate,
+    RouterLink,
+    TableModule
+  ],
   templateUrl: './role.component.html',
   styleUrl: './role.component.scss'
 })
-export class RoleComponent {
+export class RoleComponent implements OnInit{
+  cols!: Column[];
+
+  records!: any[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.cols = [
+      { field: 'role', header: 'Role' },
+    ];
+
+  }
 
 }
